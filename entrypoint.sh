acec61afc9e56e8b28cdf2246e7baaf0f4bf6329
Jim Kring <jim.kring@jki.net>
2023-01-17 23:37:23 -0800

    added more args

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,20 @@
 #!/bin/sh -l
 
 pip install -r $1
-eval $2
+
+if [ '$2' != '' ]; then
+  eval $2
+fi
+if [ '$3' != '' ]; then
+  eval $3
+fi
+if [ '$4' != '' ]; then
+  eval $4
+fi
+if [ '$5' != '' ]; then
+  eval $5
+fi
+if [ '$6' != '' ]; then
+  eval $6
+fi
+
